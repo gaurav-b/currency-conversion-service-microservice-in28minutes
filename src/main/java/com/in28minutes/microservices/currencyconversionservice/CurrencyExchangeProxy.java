c8ac9c2a6cd9a2f7f3cc717ead2fb88c213a9cc3
gaurav-b <sunymca619@gmail.com>
2022-11-10 22:53:59 +0530

    using feign client multiple instances registered with eureka and taking advantage of the client side load balancing

--- src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,8 +4,20 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange",
-				url="localhost:8000")
+/**
+ * the name below should match the name of the application 
+ * mentioned in the application.properties. Now the instances of
+ * currency-exchange-service are client side load balanced.
+ * Client side load balancing: Earlier it was done using Ribbon but now,
+ * it is done using spring cloud loadbalancer.
+ * 
+ * it is available in the class path by using "spring-cloud-starter-netflix-eureka-client"
+ * we just need to use eureka client and feign and it will provide us
+ * with client side load balancing automatically
+ *  
+ * @author gaura
+ */
+@FeignClient(name="currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
